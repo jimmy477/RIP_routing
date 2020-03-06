05c8f8f6eeeeab9f50b552a59fe704899eda3b0e
jod25 <jimmydee477@gmail.com>
2020-03-06 17:27:00 +1300

    When parser.py is run, it now reads the config.ascii file, validates it and assigns the variables.

--- parser.py
+++ parser.py
@@ -1,58 +1,70 @@
 class ConfigParser:
+    """Class that parses a configuration file for the COSC364 RIPv2 assignment"""
 
     def __init__(self, config_file):
         self.config_file = config_file
-        router_ids, input_ports, outputs, timer = self.read_file()
-        self.router_ids_line = router_ids
+        self.router_ids_line, self.input_ports_line, self.outputs_line, self.timer_line = self.read_file()
         self.router_id = self.split_ids()
-        self.input_ports_line = input_ports
-        self.outputs_line = outputs
-        self.timer_line = timer
+        self.input_ports = self.split_input_ports()
+        self.outputs = self.split_outputs()
 
     def read_file(self):
         """Reads an ascii config file and returns the lines: router_ids, input_ports,
            and timer, if it exists"""
-        timer = None
-        f = open(self.config_file, 'r')
-        lines = f.readlines()
-        router_ids = lines[0]
-        input_ports = lines[1]
-        outputs = lines[2]
 
-        # This is true if their is a timer parameter
-        if len(lines) > 3:
-            timer = lines[3]
-        f.close()
-        return router_ids, input_ports, outputs, timer
+        timer = None
+        with open(self.config_file, 'r') as f:
+            lines = f.readlines()
+            try:
+                router_ids = lines[0]
+                input_ports = lines[1]
+                outputs = lines[2]
+            except IndexError:
+                print('CONFIG_FILE ERROR: wrong formatting')
+            else:
+                # This is true if their is a timer parameter
+                if len(lines) > 3:
+                    timer = lines[3]
+                f.close()
+                return router_ids, input_ports, outputs, timer
 
     def split_ids(self):
+        """Checks the router id line is formatted correctly and then returns the router id number"""
+
+        router_ids_split = self.router_ids_line.split()
+        if router_ids_split[0] != 'router-id':
+            raise Exception('CONFIG_FILE ERROR: router-id not given')
         try:
-            return self.router_ids_line[1]
+            return router_ids_split[1]
         except IndexError:
-            print('CONFIG_FILE ERROR: No router-id given')
+            print('CONFIG_FILE ERROR: router-id not given')
 
     def split_input_ports(self):
+        """Checks the input ports line is formatted correctly and then returns the ports as a list"""
+
         ports = []
         input_ports_split = self.input_ports_line.split()
+        if input_ports_split[0] != 'input-ports':
+            raise Exception('CONFIG_FILE ERROR: input-ports not given')
         for input_port in input_ports_split[1:]:
             port = input_port.rstrip(',')
             ports.append(int(port))
-        print(ports)
+        return ports
 
     def split_outputs(self):
+        """Checks the outputs line is formatted correctly and then returns the outputs as a list"""
+
         outputs = []
         outputs_split = self.outputs_line.split()
+        if outputs_split[0] != 'outputs':
+            raise Exception('CONFIG_FILE ERROR: outputs not given')
         for input_port in outputs_split[1:]:
             output = input_port.rstrip(',')
             outputs.append(output)
-        print(outputs)
+        return outputs
 
     def split_timer(self):
         pass
-
-
-
-
 
     def split_line(self, line):
         split = line.split()
@@ -67,4 +79,3 @@ class ConfigParser:
 
 if __name__ == '__main__':
     parser = ConfigParser('config.ascii')
-    parser.split_input_ports()
