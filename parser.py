1e1fa3d20717d4e5b158de1da7418a4c8005500c
jod25 <jod25@uclive.ac.nz>
2020-04-23 18:58:39 +1200

    Created functions that make a response packet containing a routers own routing table and sends to all neighbours, a router can then unpack this packet and print the values

--- parser.py
+++ parser.py
@@ -1,14 +1,15 @@
 import json
 import select
+import struct
 import sys
 import socket
 import time
 
 localhost = '127.0.0.1'
 
+
 class ConfigParser:
     """Class that parses a configuration file for the COSC364 RIPv2 assignment"""
-
     def __init__(self):
         try:
             self.config_file = sys.argv[1]
@@ -20,16 +21,14 @@ class ConfigParser:
             self.router_id = self.split_ids()
             self.input_ports = self.split_input_ports()
             self.outputs = self.split_outputs()
-            #print(self.router_ids_line, self.router_id, self.input_ports, self.outputs)
 
     def read_file(self):
         """Reads an ascii config file and returns the lines: router_ids, input_ports,
            and timer, if it exists"""
-
         timer = None
         router_ids = None
         input_ports = None
-        outputs = None        
+        outputs = None
         with open(self.config_file, 'r') as f:
             lines = f.readlines()
             for line_n in lines:
@@ -42,10 +41,8 @@ class ConfigParser:
                     outputs = line_n
                 if param[0] == 'timer':
                     timer = line_n
-            
-   
             if router_ids is None or input_ports is None or outputs is None:
-                #checks if the parts of the ascii file have been found if not exception raised.
+                # checks if the parts of the ascii file have been found if not exception raised.
                 print('CONFIG_FILE ERROR: wrong formatting')
                 sys.exit()
             else:
@@ -55,7 +52,6 @@ class ConfigParser:
 
     def split_ids(self):
         """Checks the router id line is formatted correctly and then returns the router id number"""
-
         router_ids_split = self.router_ids_line.split()
         if router_ids_split[0] != 'router-id':
             raise Exception('CONFIG_FILE ERROR: router-id not given')
@@ -67,7 +63,6 @@ class ConfigParser:
 
     def split_input_ports(self):
         """Checks the input ports line is formatted correctly and then returns the ports as a list"""
-
         ports = []
         input_ports_split = self.input_ports_line.split()
 
@@ -89,7 +84,6 @@ class ConfigParser:
     def split_outputs(self):
         """Checks the outputs line is formatted correctly and then returns the outputs as a list of tuples of the format
            (input port num of peer router, metric to peer router, router id of peer router)"""
-
         outputs = []
         outputs_split = self.outputs_line.split()
         if outputs_split[0] != 'outputs':
@@ -107,7 +101,7 @@ class ConfigParser:
 class Router:
 
     def __init__(self, router_id, input_ports, outputs):
-        self.router_id = router_id
+        self.router_id = int(router_id)
         self.input_ports = input_ports
         self.output_ports = []
         self.outputs = outputs
@@ -117,20 +111,16 @@ class Router:
 
     def initialize_variables(self):
         """Returns a routing table from the outputs provided in the config file and also a list of all output_ports.
-           The routing table is of the form routing_table[destination router id] = (output port, cost to destination)"""
-
+           The routing table is of the form routing_table[destination router id] = (metric, next hop)"""
         routing_table = {}
         output_ports = []
         for output_port, cost, destination in self.outputs:
             output_ports.append(output_port)
-            routing_table[destination] = (output_port, cost)
+            routing_table[destination] = (cost, output_port)
         return routing_table, output_ports
-
 
     def create_udp_sockets(self):
         """Returns a list of UDP sockets, one for each input port and bound to each input port"""
-
-        # host = socket.gethostbyname(socket.gethostname())
         udp_sockets = []
         for input_port in self.input_ports:
             # TODO add  error handling
@@ -145,31 +135,71 @@ class Router:
             udp_sockets.append(input_socket)
         return udp_sockets
 
+    def create_packet(self, type):
+        """Creates the RIPv2 standard header taking in a parameter 'type' which is either 'request' or 'response'"""
+        if type == 'request':
+            command = 1
+        if type == 'response':
+            command = 2
+        try:
+            header = struct.pack(
+                'BBH',  # Specifies two unsigned ints of one byte each and one unsigned int of two bytes
+                command,
+                2,  # Specifies the version number
+                self.router_id
+            )
+            packet = header
+            for destination, values in self.routing_table.items():
+                metric, next_hop = values
+                packet = packet + struct.pack(
+                    'HHIIII',  # Specifies two unsigned ints of 2 bytes each and 4 unsigned ints of 4 bytes each
+                    2,  # Address family identifier AF_INET
+                    0,
+                    int(destination),
+                    0,
+                    0,
+                    int(metric)
+                )
+
+        except struct.error as error:
+            print(f'Error creating packet: {error}')
+        return packet
+
+    def unpack_packet(self, packet):
+        """Extracts the contents of a packet received on a input socket and returns it as a tuple of values"""
+        packet = bytes(packet)  # Needed in order to use struct.unpack
+        format = 'BBH'  # This is the format for the header
+        rip_entries = len(packet) // 20  # This gets the number of rip entries as each entry is 20 bytes
+        format += 'HHIIII' * rip_entries
+        return struct.unpack(format, packet)
+
+
     def event_loop(self):
         i = 1
         while i < 5:
             print("Waiting for event...")
-            readable, writable, exceptional = select.select(self.input_udp_sockets, [self.output_udp_socket], self.input_udp_sockets)
-            print_sockets("inputs: ", self.input_udp_sockets)
-            print_sockets("readable: ", readable)
-            print_sockets("writable: ", writable)
-            print_sockets("exceptional: ", exceptional)
+            readable, writable, exceptional = select.select(self.input_udp_sockets, [self.output_udp_socket],
+                                                            self.input_udp_sockets)
+            # print_sockets("inputs: ", self.input_udp_sockets)
+            # print_sockets("readable: ", readable)
+            # print_sockets("writable: ", writable)
+            # print_sockets("exceptional: ", exceptional)
             time.sleep(1)
             for socket in readable:
                 try:
                     msg, senders_address = socket.recvfrom(1024)
                     senders_port = senders_address[1]
-                    print(msg, senders_port)
+                    unpacked = self.unpack_packet(msg)
+                    print(unpacked)
                 except ConnectionResetError as err:
                     print(f'Error receiving packet: {err}')
             for socket in writable:
                 for output_port in self.output_ports:  # Send a update packet to each connected router
-                    print(output_port)
+                    packet = self.create_packet('response')
                     time.sleep(1)
-                    socket.sendto(bytes('hey', 'utf-8'), (localhost, int(output_port)))
+                    socket.sendto(packet, (localhost, int(output_port)))
                     time.sleep(1)
             i += 1
-
 
 def print_sockets(type, sockets):
     """Takes a list of sockets and prints their type and port numbers"""
@@ -178,7 +208,9 @@ def print_sockets(type, sockets):
         result.append(socket.getsockname()[1])
     print(type + str(result))
 
+
 if __name__ == '__main__':
     parser = ConfigParser()
     router = Router(parser.router_id, parser.input_ports, parser.outputs)
+    # router.create_packet('request')
     router.event_loop()
